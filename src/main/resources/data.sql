cbada67209c3f13b0583871c72265b9e0c1b4f1c
Ty Day <tyrday@gmail.com>
2022-06-21 20:30:00 -0400

    Add h2 in memory db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (1,'Mike', 'Smith', 'ms@mail.com');
+INSERT INTO employee (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (2,'Jacob', 'Wooler', 'ms@mail.com');
+INSERT INTO employee (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (3,'Jake', 'Ralston', 'ms@mail.com');
+INSERT INTO employee (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (4,'Meghan', 'Ralston', 'ms@mail.com');
+INSERT INTO employee (ID, FIRST_NAME, LAST_NAME, EMAIL) VALUES (5,'Ty', 'Day', 'ms@mail.com');
